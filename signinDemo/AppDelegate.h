a69e09c6f51ae675181bfdb3f1034b172ae06e38
huangping35 <huangping@me.com>
2018-05-30 09:35:08 +0800

    Initial Commit

---
+++ signinDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  signinDemo
+//
+//  Created by huangping on 2018/5/30.
+//  Copyright © 2018年 huangping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ signinDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  signinDemo
+//
+//  Created by huangping on 2018/5/30.
+//  Copyright © 2018年 huangping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
